add268a9d0aaae3bcf47372a60dcf2ff08a049e1
Nick Thompson <r.nickthompson@gmail.com>
2015-10-01 11:11:18 -0700

    added Twilio message upon registration

--- app/controllers/Loans.py
+++ app/controllers/Loans.py
@@ -1,12 +1,16 @@
 from system.core.controller import *
-
+from twilio.rest import TwilioRestClient
+account_sid = "AC5557801c4252c083b249d35b5fbef374"
+auth_token  = "3ee0d202fb03d4d0b341be99c1c19312"
+client = TwilioRestClient(account_sid, auth_token)
 
 class Loans(Controller):
     def __init__(self, action):
         super(Loans, self).__init__(action)
         self.load_model('Loan')
     def index(self):
-
+        
+       
         return self.load_view('index.html')
 
     def new_user(self):
@@ -19,7 +23,7 @@ class Loans(Controller):
         phone_number.pop(3)
         phone_number.pop(6)
         phone_number = ''.join(phone_number)
-      
+        
 
 
 
@@ -38,6 +42,16 @@ class Loans(Controller):
 
         if validate['status']:
             flash('You have successfully registered!')
+
+            print phone_number
+            phone_txt = "+1" + phone_number
+            print phone_txt
+
+            message = client.messages.create(body="Welcome Loan Share, " + new_user['first_name'] +"!",
+                to= phone_txt,    # Replace with your phone number
+                from_="+12173546021") # Replace with your Twilio number
+            print message.sid
+            
         else:
             for message in validate['errors']:
                 flash(message)
